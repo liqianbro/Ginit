031338b920800e81d4fda02a2804ee49d6ed8762
liqian <qian.li@alliance-rd.cn>
2025-05-23 15:06:57 +0800

    fix: update version banner in ginit command and remove unnecessary output in new command

--- cmd/ginit/root.go
+++ cmd/ginit/root.go
@@ -13,7 +13,7 @@ var CmdRoot = &cobra.Command{
 	Use:     "ginit",
 	Example: "ginit new demo-api",
 	Short:   "\n  ________.__       .__  __   \n /  _____/|__| ____ |__|/  |_ \n/   \\  ___|  |/    \\|  \\   __\\\n\\    \\_\\  \\  |   |  \\  ||  |  \n \\______  /__|___|  /__||__|  \n        \\/        \\/          \n\n",
-	Version: fmt.Sprintf("\n _   _                   \n| \\ | |_   _ _ __  _   _ \n|  \\| | | | | '_ \\| | | |\n| |\\  | |_| | | | | |_| |\n|_| \\_|\\__,_|_| |_|\\__,_| \n \nGinit %s - Copyright (c) 2023-2025 Ginit\nReleased under the MIT License.\n\n", config.Version),
+	Version: fmt.Sprintf("\n  ________.__       .__  __   \n /  _____/|__| ____ |__|/  |_ \n/   \\  ___|  |/    \\|  \\   __\\\n\\    \\_\\  \\  |   |  \\  ||  |  \n \\______  /__|___|  /__||__|  \n        \\/        \\/          \n\nGinit %s - Copyright (c) 2023-2025 Ginit\nReleased under the MIT License.\n\n", config.Version),
 }
 
 func init() {
